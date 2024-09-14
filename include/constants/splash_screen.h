5f0ee54dad56c6c6679ab7953d88099d9b930495
Mr L J Turton <48384584+LTurton@users.noreply.github.com>
2024-09-14 21:34:25 +0100

    CPP:S5421 - global variables should be const Also has cpp:S5845 but I assume that's a capability you didn't port yet

--- include/constants/splash_screen.h
+++ include/constants/splash_screen.h
@@ -5,8 +5,7 @@
 #ifndef SPLASH_SCREEN_H
 #define SPLASH_SCREEN_H
 
-
 #include "types.h"
-inline u32 load_bar_region[4] = {248, 483, 534, 24};
-extern u32 SPLASH_DATA[1024*768];
+inline const u32 load_bar_region[4] = { 248, 483, 534, 24 };
+extern const u32 SPLASH_DATA[1024 * 768];
 #endif //SPLASH_SCREEN_H
