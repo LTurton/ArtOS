f674086113e9ca6356ef44602a94d32a18f79b59
Mr L J Turton <48384584+LTurton@users.noreply.github.com>
2024-09-14 21:29:58 +0100

    typedef to using

--- include/constants/types.h
+++ include/constants/types.h
@@ -1,16 +1,16 @@
 /*
 This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 #ifndef __TYPES_H_
 #define __TYPES_H_
@@ -21,15 +21,14 @@ This program is free software: you can redistribute it and/or modify
 /*
   define some specific length types
 */
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
-
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
 
 // Helper functions for templating
 
@@ -43,22 +42,22 @@ constexpr bool is_any_of_v = (is_same_v<T, Ts> || ...);
 
 template <typename T>
 struct remove_const_volatile {
-    using type = T;
+	using type = T;
 };
 
 template <typename T>
 struct remove_const_volatile<volatile T> {
-    using type = T;
+	using type = T;
 };
 
 template <typename T>
 struct remove_const_volatile<const T> {
-    using type = T;
+	using type = T;
 };
 
 template <typename T>
-struct remove_const_volatile<const volatile T>{
-using type = T;
+struct remove_const_volatile<const volatile T> {
+	using type = T;
 };
 
 template<typename T>
@@ -66,27 +65,24 @@ using remove_cv_t = typename remove_const_volatile<T>::type;
 
 template<typename Ty>
 constexpr bool is_int_like_v = is_any_of_v<
-    remove_cv_t<Ty>,
-    char,
-    signed char,
-    unsigned char,
-    wchar_t,
-    char16_t,
-    char32_t,
-    short,
-    unsigned short,
-    int,
-    unsigned int,
-    long,
-    unsigned long,
-    long long,
-    unsigned long long
+	remove_cv_t<Ty>,
+	char,
+	signed char,
+	unsigned char,
+	wchar_t,
+	char16_t,
+	char32_t,
+	short,
+	unsigned short,
+	int,
+	unsigned int,
+	long,
+	unsigned long,
+	long long,
+	unsigned long long
 >;
 
 template <class T>
 concept int_like = is_int_like_v<T>;
-
-
-
 
 #endif
