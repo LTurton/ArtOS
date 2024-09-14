5f5862d1f08a6a36ee5801f2ed52bc39bf827c33
Mr L J Turton <48384584+LTurton@users.noreply.github.com>
2024-09-14 22:18:12 +0100

    errors --> enum

--- include/constants/Errors.h
+++ include/constants/Errors.h
@@ -4,13 +4,13 @@
 
 #ifndef ERRORS_H
 #define ERRORS_H
+enum Errors {
+	NO_ERROR,
+	INVALID_ARG,
+	NO_MEMORY,
+	NOT_IMPLEMENTED,
+	NOT_FOUND,
+	DEVICE_ERROR = 8
+};
 
-
-#define NO_ERROR 0
-#define INVALID_ARG 1
-#define NO_MEMORY 2
-#define NOT_IMPLEMENTED 3
-#define NOT_FOUND 4
-
-#define DEVICE_ERROR 8
 #endif //ERRORS_H
